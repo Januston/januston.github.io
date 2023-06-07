2afe69e995e58d0a1ee9b23e58b847971b1f8b76
januston <januston.nicholas@outlook.com>
2023-06-07 12:11:48 -0400

    update resume

--- src/config.mjs
+++ src/config.mjs
@@ -7,9 +7,9 @@ const CONFIG = {
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'AstroWind — Free template for create a website with Astro + Tailwind CSS',
+  title: 'januston.github.io',
   description:
-    '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
+    'Website made using Astrowind🚀',
   defaultImage: defaultImage,
 
   defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
